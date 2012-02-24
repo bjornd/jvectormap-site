d33a432195c5369c9c29d41b1b20a8c53bc639bf
Kirill Lebedev <echo.bjornd@gmail.com>
2012-02-24 13:10:21 +0600

    Added chaching of the generated maps based on MD5 hash of the converter parameters

--- lib/default.rb
+++ lib/default.rb
@@ -4,25 +4,30 @@
 def map_preprocessing(item)
   params = item[:map_params]
   data_file = params.delete(:data_file)
-  output_file = 'jquery-jvectormap-'+params[:name]+'-'+(params[:language] ? params[:language] : 'en')
-  output_file_path = 'tmp/'+output_file+'.js'
   
   converter_params = []
   params.each { |key, value| converter_params << '--'+key.to_s+' "'+value.to_s+'"' }
   
-  system(
-    'python '+
-    @config[:jvectormap_path]+'/converter/converter.py '+
-    data_file+' '+
-    output_file_path+' '+
-    converter_params.join(' ')
-  )
+  map_id = Digest::MD5.hexdigest(data_file+converter_params.join)
+  map_name = 'jquery-jvectormap-'+params[:name]+'-'+(params[:language] ? params[:language] : 'en')
+  output_file_path = 'tmp/'+map_id+'.js'
+  
+  if !File.exists? output_file_path  
+    converter_command = 
+        'python '+
+        @config[:jvectormap_path]+'/converter/converter.py '+
+        data_file+' '+
+        output_file_path+' '+
+        converter_params.join(' ')
+
+    system(converter_command)
+  end
   
   @items << Nanoc3::Item.new(
     File.open(output_file_path, "r").read,
     {},
-    "/js/"+output_file+'/'
+    "/js/"+map_name+'/'
   )
-  
-  item[:js_assets] = ['/js/'+output_file+'.js']
+
+  item[:js_assets] = ['/js/'+map_name+'.js']
 end
