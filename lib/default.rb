09163c6d4d381220b871f20b19e1fb616d0c943b
Kirill Lebedev <echo.bjornd@gmail.com>
2012-03-18 18:03:26 +0600

    fixes #2 Page of the map should include file size, link to data source, map name.

--- lib/default.rb
+++ lib/default.rb
@@ -4,16 +4,16 @@
 def map_preprocessing(item)
   params = item[:map_params]
   data_file = params.delete(:data_file)
-  
+
   converter_params = []
   params.each { |key, value| converter_params << '--'+key.to_s+" '"+value.to_s+"'" }
-  
+
   map_id = Digest::MD5.hexdigest(data_file+converter_params.join)
   map_name = 'jquery-jvectormap-'+params[:name]+'-'+(params[:language] ? params[:language] : 'en')
   output_file_path = 'tmp/'+map_id+'.js'
-  
-  if !File.exists? output_file_path  
-    converter_command = 
+
+  if !File.exists? output_file_path
+    converter_command =
         'python '+
         @config[:jvectormap_path]+'/converter/converter.py '+
         data_file+' '+
@@ -22,12 +22,27 @@ def map_preprocessing(item)
 
     system(converter_command)
   end
-  
+
   @items << Nanoc3::Item.new(
     File.open(output_file_path, "r").read,
     {},
     "/js/"+map_name+'/'
   )
 
+  item[:map_params][:download_link] = '/js/'+map_name+'.js'
+  item[:map_params][:file_size] = File.size output_file_path
+
   item[:js_assets] = ['/js/'+map_name+'.js']
+end
+
+#converts number of bytes to human-readable format, code is from
+#http://www.ruby-forum.com/topic/92075
+def numer_to_human_size(size)
+  prefix = %W(TB GB MB KB B)
+  i = prefix.length - 1
+  while size > 512 && i > 0
+    size /= 1024
+    i -= 1
+  end
+  ((size > 9 || size.modulo(1) < 0.1 ? '%d' : '%.1f') % size) + ' ' + prefix[i]
 end
